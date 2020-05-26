d83d518cafe349af048a49a01674e8bdcc93ea35
JasonNelemans <jasonnelemans@hotmail.com>
2020-05-26 16:33:35 +0200

    different types

--- type.js
+++ type.js
@@ -2,3 +2,20 @@ function hello(world) {
     console.log('Hello ' + world);
 }
 hello('Jon');
+var isWinter = false;
+var number = 5;
+var weird = 'Man';
+var names = ['Jon', 'Cena', 'Wayn'];
+var any = ['Maya', 1, true, null];
+var Starks;
+(function (Starks) {
+    Starks[Starks["Jon"] = 0] = "Jon";
+    Starks[Starks["Brendan"] = 1] = "Brendan";
+    Starks[Starks["Eddard"] = 2] = "Eddard";
+    Starks[Starks["Catlyn"] = 3] = "Catlyn";
+})(Starks || (Starks = {}));
+;
+var cat = Starks.Catlyn;
+function getName() {
+    console.log('winter is coming');
+}
--- type.ts
+++ type.ts
@@ -3,3 +3,18 @@ function hello (world: string) {
 }
 
 hello('Jon');
+
+const isWinter : boolean = false;
+
+const number : number = 5;
+const weird : string = 'Man';
+
+const names : string[] = ['Jon','Cena','Wayn'];
+const any : any[] = ['Maya', 1, true, null];
+
+enum Starks {Jon,Brendan,Eddard,Catlyn};
+var cat : Starks = Starks.Catlyn;
+
+function getName(): any {
+  console.log('winter is coming');
+}
